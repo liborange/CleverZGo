8beb5c7df1ce60beb4fc96d17c183f095d8e899b
hzlibaocheng <hzlibaocheng@corp.netease.com>
2016-12-09 10:39:12 +0800

    Test: test usable

---
+++ src/main/java/RunningMan.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by liborange on 15/12/18.
+ */
+public class RunningMan {
+
+    public static void main(String[] args) {
+        float i = 0.2f;
+        System.out.println(-i%0.8);
+    }
+
+}
